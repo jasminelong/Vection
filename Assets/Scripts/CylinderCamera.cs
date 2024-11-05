0741a5ba14bcf0ecc49830b592c057c9d0b92aea
jasminelong <jasmineeven2@gmail.com>
2024-11-05 16:33:29 +0900

    add grayimage

--- Assets/Scripts/CylinderCamera.cs
+++ Assets/Scripts/CylinderCamera.cs
@@ -15,7 +15,9 @@ public class CylinderCamera : MonoBehaviour
     } 
     public Pattern movementPattern; // イメージの提示パターン // 图像提示的模式
     public Camera userCamera; // 連続運動のカメラ // 连续运动的摄像机
-    public Camera captureCamera; // 一定の距離ごとに写真を撮るためのカメラ // 用于间隔一定距离拍照的摄像机
+    public Camera captureCamera;
+    public Camera captureCamera1; // 一定の距離ごとに写真を撮るためのカメラ // 用于间隔一定距离拍照的摄像机
+    public Camera captureCamera2; // 一定の距離ごとに写真を撮るためのカメラ // 用于间隔一定距离拍照的摄像机
     public Vector3 cylinderTopCenter; // 円柱の頂点の中心位置 // 圆柱顶部的中心位置
     public float cameraSpeed = 1f; // カメラが円柱の軸に沿って移動する速度 (m/s) // 摄像机沿圆柱轴线移动的速度，m/s
     public float fps = 60f; // 他のfps // 其他的fps
@@ -23,16 +25,12 @@ public class CylinderCamera : MonoBehaviour
     private float trialTime = 3 * 60 * 1000f;//实验的总时间
     private float captureIntervalDistance; // 撮影間隔の距離 (m) // 拍摄间隔距离，m
     private GameObject canvas;
-    private Transform cameraTransform; // メインカメラのTransform // 主摄像机的Transform
     private Transform displayImageTransform;
     private Transform preImageTransform;
     private Transform nextImageTransform;
     private RawImage displayImageRawImage;// 撮影した画像を表示するためのUIコンポーネント // 用于显示拍摄图像的UI组件
     private RawImage preImageRawImage;// 撮影した画像を表示するためのUIコンポーネント // 用于显示拍摄图像的UI组件
     private RawImage nextImageRawImage;// 撮影した画像を表示するためのUIコンポーネント // 用于显示拍摄图像的UI组件
-    private RectTransform capturedImageRect;
-    private int capturedImageWidth;
-    private int capturedImageHeight;
     private float cylinderHeight; // 円柱の高さ (m) // 圆柱的高度，m
 
     private List<(Texture2D, Vector3)> capturedImages; // 画像と位置を格納するリスト // 存储图片和位置的列表
@@ -52,7 +50,6 @@ public class CylinderCamera : MonoBehaviour
     private bool vectionResponse = false;
     private string folderName = "ExperimentData"; // サブフォルダ名 // 子文件夹名称
     private float timeMs; // 現在までの経過時間 // 运行到现在的时间
-    private int preFrameNum = 0;
     private Vector3 direction;
     private float bufferDurTime = 10200f;
     public Image grayImage;
@@ -64,8 +61,6 @@ public class CylinderCamera : MonoBehaviour
         QualitySettings.vSyncCount = 0;
         // 目標フレームレートを60フレーム/秒に設定 // 设置目标帧率为60帧每秒
         Time.fixedDeltaTime = 1.0f / 60.0f;
-        //userCamera.transform.position = new Vector3(0f, 0f, 0f) ; // カメラの初期位置を円柱の底部中心に設定 // 相机初始位置设为圆柱底部中心
-        //cameraTransform = userCame111111ra.transform; // メインカメラのTransformを取得 // 获取主摄像机的Transform
         cylinderTopCenter = new Vector3(0f, 0f, 1400f); // 円柱の頂点位置を高さの頂点に設定 // 圆柱顶部位置设为高度的顶点
 
        // captureCamera.enabled = false; // 初期状態でキャプチャカメラを無効にする // 初始化时禁用捕获摄像机
@@ -84,13 +79,13 @@ public class CylinderCamera : MonoBehaviour
             case Pattern.wobble:
                 data.Add("FrameNum, Time [ms], Vection Response (0:no, 1: yes )");
                 displayImageRawImage.enabled = true;
-                //CaptureImagesAtIntervalsSave();
                 break;
             case Pattern.luminanceMixture:
-                data.Add("FrondFrameNum, FrondFrameLuminance, BackFrameNum, BackFrameLuminance, Time [ms], Vection Response (0:no, 1: yes )");
+                data.Add("FrondFrameNum, FrondFrameLuminance, BackFrameNum, BackFrameLuminance, Time, Vection Response");
+                frameNum++;
                 preImageRawImage.enabled = true;
                 nextImageRawImage.enabled = true;
-                //CaptureImagesAtIntervalsSave();
+                captureCamera2.transform.position = new Vector3(0f, 0f , captureIntervalDistance);
                 break;
         }
 
@@ -121,7 +116,7 @@ public class CylinderCamera : MonoBehaviour
         switch (movementPattern)
         {
             case Pattern.continuous:
-                MoveCamera();
+                Continuous();
                 break;
             case Pattern.wobble:
                 Wabble();
@@ -130,9 +125,6 @@ public class CylinderCamera : MonoBehaviour
                 LuminanceMixture();
                 break;
         }
-        //MoveCamera();
-        // 每一帧保存当前相机的内容为图片
-        //SaveFrame();
     }
 
     void GetRawImage()
@@ -152,97 +144,81 @@ public class CylinderCamera : MonoBehaviour
         displayImageRawImage.enabled = false;
         preImageRawImage.enabled = false;
         nextImageRawImage.enabled = false;
-
-        // RawImageの幅と高さを取得 // 获取 RawImage 的宽高
-        capturedImageRect = displayImageTransform.GetComponent<RectTransform>();
-        capturedImageWidth = (int)capturedImageRect.rect.width;
-        capturedImageHeight = (int)capturedImageRect.rect.height;
     }
 
 
-    void MoveCamera()
+    void Continuous()
     {
-        if (timeMs >= (trialTime+2* bufferDurTime))
+        if (timeMs < bufferDurTime)
+        {
+            // データを記録 // 记录数据
+            data.Add($"0, {timeMs - bufferDurTime:F5}, {(vectionResponse ? 1 : 0)}");
+        }
+        else if (timeMs >= bufferDurTime && timeMs <= (bufferDurTime + trialTime))
+        {
+            displayImageRawImage.enabled = true;
+            // カメラが円柱の軸に沿って移動する目標位置を計算 // 计算摄像机沿圆锥轴线移动的目标位置
+            Vector3 targetPosition = captureCamera.transform.position + direction * cameraSpeed * Time.deltaTime;
+            // カメラを目標位置に移動 // 移动摄像机到目标位置
+            captureCamera.transform.position = targetPosition;
+
+            // カメラを常に円柱の頂点に向ける // 确保摄像机始终朝向圆锥顶点
+            captureCamera.transform.LookAt(cylinderTopCenter);
+            frameNum++;
+            // データを記録 // 记录数据,这里是为了记录数据从1开始，所以用的frameNum而不是frameNum-1,因为list的下标是从0开始的
+            data.Add($"{frameNum}, {timeMs - bufferDurTime:F4}, {(vectionResponse ? 1 : 0)}");
+        }
+        else if (timeMs > (bufferDurTime + trialTime) && timeMs <= (trialTime + 2 * bufferDurTime))
+        {
+            displayImageRawImage.enabled = false;
+            StartCoroutine(ShowGrayScreen(bufferDurTime / 1000));
+            data.Add($"0, {timeMs - bufferDurTime:F5}, {(vectionResponse ? 1 : 0)}");
+        }
+        else if (timeMs > (trialTime + 2 * bufferDurTime))
         {
             QuitGame();
         }
-        if (timeMs >= bufferDurTime)
-        {
-            displayImageRawImage.enabled = true;
-        }
-        if (timeMs >= (bufferDurTime + trialTime))
-        {
-            displayImageRawImage.enabled = false;
-        }
-        // カメラが円柱の軸に沿って移動する目標位置を計算 // 计算摄像机沿圆锥轴线移动的目标位置
-        Vector3 targetPosition = captureCamera.transform.position + direction * cameraSpeed * Time.deltaTime;
-        // カメラを目標位置に移動 // 移动摄像机到目标位置
-        captureCamera.transform.position = targetPosition;
-
-        // カメラを常に円柱の頂点に向ける // 确保摄像机始终朝向圆锥顶点
-        captureCamera.transform.LookAt(cylinderTopCenter);
-        frameNum++;
-        // データを記録 // 记录数据,这里是为了记录数据从1开始，所以用的frameNum而不是frameNum-1,因为list的下标是从0开始的
-        data.Add($"{frameNum}, {timeMs- bufferDurTime:F4}, {(vectionResponse ? 1 : 0)}");
     }
     void Wabble()
     {
-        var Image = capturedImages[frameNum]; // 画像を取得 // 获取一个元素
-        Texture2D Texture = Image.Item1; // Texture2Dを取得 // 获取Texture2D
-
-        if (Mathf.Abs(timeMs - frameNum * updateInterval * 1000) < 0.01f)
-        {
-            displayImageRawImage.texture = Texture;
-            frameNum++;
-        }
-
         // データを記録 // 记录数据,这里是为了记录数据从1开始，所以用的frameNum而不是frameNum-1,因为list的下标是从0开始的
         data.Add($"{frameNum}, {timeMs:F4}, {(vectionResponse ? 1 : 0)}");
     }
-
-    void CaptureImagesAtIntervalsSave()
-    {
-        for (float z = 0; z <=1400 ; z += captureIntervalDistance)
-        {
-            captureCamera.transform.position = new Vector3(0, 0, z);
-
-            // 画像と対応する位置を保存 // 保存图像及其位置
-            capturedImages.Add((CaptureRenderTexture(), captureCamera.transform.position));
-        }
-    }
-
     void LuminanceMixture()
     {
-        if (frameNum < capturedImages.Count - 1)
+        if (timeMs < bufferDurTime)
         {
+            // データを記録 // 记录数据
+            data.Add($"0, 0, 0, 0, {timeMs - bufferDurTime:F5}, {(vectionResponse ? 1 : 0)}");
+        }
+        else  if (timeMs >= bufferDurTime && timeMs <= (bufferDurTime + trialTime))
+        {
+            preImageRawImage.enabled = true;
+            nextImageRawImage.enabled = true;
             // 写真を撮る距離に達したかをチェック // 检查是否到了拍照的距离
-            if (Mathf.Abs(timeMs - (frameNum + 1) * updateInterval * 1000) < 0.01f)
+            if (Mathf.Abs((timeMs - bufferDurTime) - (frameNum + 1) * updateInterval * 1000) < 0.01f)
             {
+                // カメラが円柱の軸に沿って移動する目標位置を計算 // 计算摄像机沿圆锥轴线移动的目标位置
+                Vector3 targetPosition = direction * cameraSpeed * updateInterval;
+                Vector3 targetPosition1 = captureCamera1.transform.position + targetPosition;
+                Vector3 targetPosition2 = captureCamera2.transform.position + targetPosition;
+                // カメラを目標位置に移動 // 移动摄像机到目标位置
+                captureCamera1.transform.position = targetPosition1;
+                captureCamera2.transform.position = targetPosition2;
+
+                // カメラを常に円柱の頂点に向ける // 确保摄像机始终朝向圆锥顶点
+                captureCamera1.transform.LookAt(cylinderTopCenter);
+                captureCamera2.transform.LookAt(cylinderTopCenter);
                 frameNum++;
             }
-
-            var previousImage = capturedImages[frameNum]; // 手前の画像を取得 // 获取前一个元素
-            Texture2D previousTexture = previousImage.Item1; // Texture2Dを取得 // 获取Texture2D
-            Vector3 previousPosition = previousImage.Item2; // 位置を取得 // 获取位置
-
-            var nextImage = capturedImages[frameNum+1]; // 次の画像を取得 // 获取后一个元素
-            Texture2D nextTexture = nextImage.Item1; // Texture2Dを取得 // 获取Texture2D
-
-            //  手前の画像から現在のカメラまでの距離を計算 // 计算前一张图片到正在运动的相机的距离
-            float preImageToCameraCurrentDistance = Vector3.Distance(previousPosition, cameraTransform.position);
-
-            //  手前の画像と次の画像の輝度値を計算 // 计算前一张和后一张图片的辉度值
-            float nextRatio = preImageToCameraCurrentDistance / (captureIntervalDistance);
+            float preImageToNowDeltaTime = timeMs - bufferDurTime - (frameNum - 1) * updateInterval * 1000;
+            float nextRatio = preImageToNowDeltaTime / (updateInterval * 1000);
             float nextImageRatio = nextRatio > 1.0f ? 1.0f : nextRatio;
             float previousImageRatio = 1.0f - nextImageRatio;
-
-            // 変更された色をRawImageに適用 // 将修改后的颜色应用到 RawImage
-            if (preFrameNum != frameNum)
-            {
-                preFrameNum = frameNum;
-                preImageRawImage.texture = previousTexture;
-                nextImageRawImage.texture = nextTexture;
-            }
+            /*Color preImageColor = preImageRawImage.color;
+            preImageColor.a = previousImageRatio;
+            Color nextImageColor = preImageRawImage.color;
+            nextImageColor.a = nextImageRatio;*/
             preImageRawImage.color = new Color(preImageRawImage.color.r, preImageRawImage.color.g, preImageRawImage.color.b, previousImageRatio);
             nextImageRawImage.color = new Color(nextImageRawImage.color.r, nextImageRawImage.color.g, nextImageRawImage.color.b, nextImageRatio);
             // Canvasに親オブジェクトを設定し、元のローカル位置、回転、およびスケールを保持 // 设置父对象为 Canvas，并保持原始的本地位置、旋转和缩放
@@ -251,34 +227,22 @@ public class CylinderCamera : MonoBehaviour
 
 
             // データを記録 // 记录数据
-            data.Add($"{frameNum + 1}, {previousImageRatio:F5},{frameNum + 2}, {nextImageRatio:F5},{timeMs:F5}, {(vectionResponse ? 1 : 0)}");
+            data.Add($"{frameNum}, {previousImageRatio:F5}, {frameNum + 1}, {nextImageRatio:F5}, {timeMs - bufferDurTime:F5}, {(vectionResponse ? 1 : 0)}");
+        }
+        else if (timeMs > (bufferDurTime + trialTime) && timeMs <= (trialTime + 2 * bufferDurTime))
+        {
+            preImageRawImage.enabled = false;
+            nextImageRawImage.enabled = false;
+            StartCoroutine(ShowGrayScreen(bufferDurTime / 1000));
+            // データを記録 // 记录数据
+            data.Add($"0, 0, 0, 0, {timeMs - bufferDurTime:F5}, {(vectionResponse ? 1 : 0)}");
+        }
+        else if (timeMs > (trialTime + 2 * bufferDurTime))
+        {
+            QuitGame();
         }
     }
-    Texture2D CaptureRenderTexture()
-    {
-        // RenderTextureを作成し、キャプチャカメラのターゲットとして設定 // 创建一个RenderTexture并将其设置为捕获摄像机的目标
-        RenderTexture rt = TexturePool.Instance.GetRenderTextureFromPool(capturedImageWidth, capturedImageHeight);
-        captureCamera.targetTexture = rt;
 
-        // 画像をレンダリング // 渲染图像
-        captureCamera.Render();
-
-        // RenderTextureをアクティブ化 // 激活 RenderTexture
-        RenderTexture.active = rt;
-
-        // 新しいTexture2Dを作成 // 创建一个新的 Texture2D
-        Texture2D capturedImage = TexturePool.Instance.GetTexture2DFromPool(rt.width, rt.height, TextureFormat.RGB24);
-
-        // RenderTextureからピクセルデータをTexture2Dに読み込む // 从 RenderTexture 中读取像素数据到 Texture2D
-        capturedImage.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
-        capturedImage.Apply();
-
-        // 解放 // 释放
-        captureCamera.targetTexture = null;
-        RenderTexture.active = null;
-        TexturePool.Instance.ReturnRenderTextureToPool(rt);
-        return capturedImage;
-    }
     void QuitGame()
     {
 #if UNITY_EDITOR
